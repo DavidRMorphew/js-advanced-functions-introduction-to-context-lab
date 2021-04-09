0ab652cbb3b14f5852baa0bdbf921460d2d3aa98
DavidRMorphew <morphewd@umich.edu>
2021-04-09 10:16:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+const createEmployeeRecord = ([firstName, familyName, title, payPerHour]) => {
+    let newRecord = Object.assign({}, {firstName, familyName, title, payPerHour, timeInEvents: [], timeOutEvents: []});
+    return newRecord;
+};
+
+const createEmployeeRecords = (arrayOfArrays) => {
+    return arrayOfArrays.map(createEmployeeRecord);
+};
+
+const createTimeInEvent = (employeeRecordObj, dateStamp) => {
+    let [date, hour] = dateStamp.split(" ")
+    hour = parseInt(hour);
+    const timeInObj = {type: "TimeIn", hour, date}
+    employeeRecordObj.timeInEvents.push(timeInObj);
+    return employeeRecordObj;
+};
+
+const createTimeOutEvent = (employeeRecordObj, dateStamp) => {
+    let [date, hour] = dateStamp.split(" ")
+    hour = parseInt(hour);
+    const timeOutObj = {type: "TimeOut", hour, date}
+    employeeRecordObj.timeOutEvents.push(timeOutObj);
+    return employeeRecordObj;
+};
+
+const hoursWorkedOnDate = (employeeRecordObj, date) => {
+    const timeInOnDate = employeeRecordObj.timeInEvents.find((obj) => {return obj.date === date}).hour
+    const timeOutOnDate = employeeRecordObj.timeOutEvents.find((obj) => {return obj.date === date}).hour
+    return (timeOutOnDate - timeInOnDate)/100;
+};
+
+const wagesEarnedOnDate = (employeeRecordObj, date) => {
+    const hoursWorked = hoursWorkedOnDate(employeeRecordObj, date);
+    return hoursWorked * employeeRecordObj.payPerHour;
+};
+
+const allWagesFor = (employeeRecordObj) => {
+
+};
+
+const findEmployeeByFirstName = () => {};
+
+const calculatePayroll = () => {};
